32ac4ed09b4a258afe47d12858cc3c618971a3ef
Josue <sjosue5082002@gmail.com>
2022-03-18 19:40:39 -0600

    Estilo del componente HeroScreen

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,20 +1,43 @@
-import { useParams, Navigate} from "react-router-dom"
+import { useParams, Navigate, useNavigate} from "react-router-dom"
 import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
 
   const {heroeId} = useParams();
+  const navigate = useNavigate();
+
   const hero = getHeroById(heroeId);
+
+  const handleReturn = () => {
+    navigate(`/${hero.publisher.split(' ')[0].toString().toLowerCase()}`);
+  }
 
   if (!hero) {
     return <Navigate to="/" />;
-  }else{
-    return (
-      <div>
-          <h1>HeroScreen</h1>
-          <hr/>
-          <h2>{hero.superhero}</h2>
-      </div>
-    )
   }
+
+  const {superhero, publisher, alter_ego, first_appearance, characters} = hero;
+  const imagePath = `/assets/${heroeId}.jpg`;
+  return (
+    <div className="row mt-5">
+        <div className="col-4">
+          <img src={imagePath} className="img-thumbnail" alt={superhero} />
+        </div>
+        <div className="col-8">
+          <h2>{superhero}</h2>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
+            <li className="list-group-item"><b>Publisher: </b>{publisher}</li>
+            <li className="list-group-item"><b>First appearance: </b>{first_appearance}</li>
+          </ul>
+          <h5 className="mt-3">Characters</h5>
+          <p>{characters}</p>
+
+          <button className="btn btn-outline-info" onClick={handleReturn}>
+            Regresar
+          </button>
+        </div>
+    </div>
+  )
+  
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HeroesApp } from './HeroesApp';
 
+document.getElementById('root').style.backgroundColor = '#efecec';
+
 ReactDOM.render(
     <HeroesApp/>,
   document.getElementById('root')
+  
 );
